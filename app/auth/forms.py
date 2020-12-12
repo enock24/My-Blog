c5b63f58618b3bc3c947510a771170b8488bf1d4
enock24 <enookimuu@gmail.com>
2020-12-13 00:51:09 +0300

    add form to auth folder

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Required,Email,EqualTo
-from ..models import User
-from wtforms import ValidationError
+from wtforms.validators import Required,Email,EqualTo,ValidationError
+from ..models import User,Writer
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
+
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
@@ -13,16 +13,36 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_email(self,data_field):
-            if User.query.filter_by(email =data_field.data).first():
-                raise ValidationError('There is an account with that email')
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
 
     def validate_username(self,data_field):
-        if User.query.filter_by(author = data_field.data).first():
+        if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+        
+class WriterRegistrationForm(FlaskForm):
+    writer_email = StringField('Your Email Address',validators=[Required(),Email()])
+    writer_name = StringField('Enter your username',validators = [Required()])
+    writer_password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Writer Sign Up')
 
+    def validate_email(self,data_field):
+        if Writer.query.filter_by(writer_email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if Writer.query.filter_by(writer_name = data_field.data).first():
+            raise ValidationError('That writer name is taken')
 
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+    
+class WriterLoginForm(FlaskForm):
+    writer_email = StringField('Writer Email Address',validators=[Required(),Email()])
+    password = PasswordField('Writer Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
